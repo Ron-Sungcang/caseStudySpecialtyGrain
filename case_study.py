f8f68d2ab592b1369477c20453a73eef92377b46
Ron-Sungcang <sungcangjoseph@gmail.com>
2024-05-19 18:06:39 -0600

    Prepared the environment and viewed the dataframes

---
+++ case_study.py
@@ -0,0 +1,11 @@
+#Preparing the environment
+import sqlite3 as sql
+import pandas as pd
+import matplotlib as plt
+import datetime as dt
+
+grain_df = pd.read_excel('merged_specialty_grain_revenue.xlsx')
+print(grain_df.head())
+
+weather_df = pd.read_csv('en_climate_monthly_SK_estevan.csv')
+print(weather_df.head())
