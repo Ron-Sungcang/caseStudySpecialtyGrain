7de98f231a643cb0161b52a2ce9629d1fd066d31
Ron-Sungcang <sungcangjoseph@gmail.com>
2024-05-20 15:16:53 -0600

    added sql functionalities. Put all spreadsheets in db

--- case_study.py
+++ case_study.py
@@ -1,14 +1,22 @@
-#Preparing the environment
+# Preparing the environment
 import sqlite3 as sql
 import pandas as pd
 import matplotlib as plt
 import datetime as dt
 
-grain_df = pd.read_excel('merged_specialty_grain_revenue.xlsx')
-print(grain_df.head())
+# connect to database
+con = sql.connect("grains.db")
 
-weather_df = pd.read_csv('en_climate_monthly_SK_estevan.csv')
-print(weather_df.head())
+# create a cursor
+cur = con.cursor()
 
-grain_df.info()
-weather_df.info()
+pd.read_excel('merged_specialty_grain_revenue.xlsx').to_sql("specialty_grains",con,if_exists='append',index=False)
+pd.read_csv('en_climate_monthly_SK_estevan.csv').to_sql("historical_weather",con,if_exists='append',index=False)
+
+
+grains_df = pd.read_sql(f'SELECT * FROM {"specialty_grains"}',con)
+weather_df = pd.read_sql(f'SELECT * FROM {"historical_weather"}',con)
+# checking how many tables in db
+cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
+print(cur.fetchall())
+
